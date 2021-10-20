258c6b844b0a7a3d76535dda9b769c45ee7dafcd
Martin <Martin.Gruber.1112@gmail.com>
2021-10-20 13:36:53 +0200

    added branch parameter

--- R/update_package.R
+++ R/update_package.R
@@ -3,24 +3,29 @@
 #' @param repo user/repo name
 #' @param token_file file with token
 #' @param token auth_token for devtools::install_github
+#' @param branch name of branch to update from
 #'
 #' @examples
 #' update_package()
-#' update_package(repo = "User/Repo_Name", token = "your_token")
+#' update_package(repo = "User/Repo_Name", token = "your_token", branch = "shiny")
 #'
 #' @importFrom devtools install_github
 #' @export
 
 
-update_package <- function(repo = "elMrt/auto-statistics", token_file, token){
+update_package <- function(repo = "elMrt/auto-statistics", token_file, token, branch = "main"){
   if(!missing(token_file)){
     token <- readLines(token_file, warn = FALSE)
+    # read branch from second Line -> token has length of 2
+    if(length(token) != 1){
+      branch = token[2]
+      token = token[1]
+    }
   }
   tryCatch({
-    devtools::install_github(repo = repo, auth_token = token)
+    devtools::install_github(repo = repo, auth_token = token, ref = branch)
   },
   error=function(cond) {
     message(cond)
-  }
-  )
+  })
 }
