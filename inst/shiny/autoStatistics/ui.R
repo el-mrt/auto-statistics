03721acb28b2b30fda33c73423c6cf5654307c5a
elMrt <Martin.Gruber.1112@gmail.com>
2021-10-22 02:37:08 +0200

    initial

--- inst/shiny/autoStatistics/ui.R
+++
@@ -1,9 +0,0 @@
-library("shiny")
-
-#ui <- navbarPage("App Title",
- #                tabPanel("Data"),
-  #               tabPanel("Placeholder"))
-
-
-ui <- navbarPage("App Title",
-                 tabPanel("Data",file_input_ui("my_file_ui")))
