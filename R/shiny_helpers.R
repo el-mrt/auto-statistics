a38d0e858c11927c86e83c76557a97377e88406c
elMrt <Martin.Gruber.1112@gmail.com>
2021-11-03 08:48:50 +0100

    moved run_shiny function into shiny_helpers

--- R/shiny_helpers.R
+++ R/shiny_helpers.R
@@ -17,3 +17,19 @@ insert_line_break <- function(x, n = 30){
   new_string <- stringr::str_replace_all(x, paste0("(.{",n,"})"), "\\1\n")
   return(new_string)
 }
+
+
+#' Function to run the provided shiny App in this package
+#'
+#'
+#' @importFrom shiny runApp
+#' @export
+
+
+run_shiny <- function(){
+  shiny_path <- system.file("shiny", "autoStatistics", package = "autoStatistics")
+  if(shiny_path == "")
+    stop("couldnt find the path to the shiny application. Try reinstalling `autoStatistics`", .call = FALSE)
+
+  shiny::shinyAppDir(shiny_path)
+}
