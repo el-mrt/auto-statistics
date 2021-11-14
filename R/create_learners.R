421febd1cd6e3f216d08f1d8e0ac97d4a45407ce
Lukas Unterschütz <unterschuetz.lukas@gmail.com>
2021-11-14 11:56:29 +0100

    wrote hpo extention for learner funciton

--- R/create_learners.R
+++ R/create_learners.R
@@ -2,10 +2,12 @@
 #'
 #' @param task mlr3 task
 #' @param vec_learners vector of choosen learners
+#' @param hpo choose if searchspaces for hyperparameter optimization should be initialized
 #'
 #' @examples
 #' create_learners(tsk("iris"))
 #' create_learners(tsk("bike_sharing"))
+#' create_learners(task = tsk("german_credit"), vec_learners = c("rf"), hpo = FALSE)
 #'
 #' @return list of mlr3 learners
 #'
@@ -14,7 +16,7 @@
 #' @import mlr3verse
 #'
 
-create_learners <- function(task, vec_learners = NULL){
+create_learners <- function(task, vec_learners = NULL, hpo = TRUE){
   cr <- task$task_type
 
   make_name <- function(cr, lrn){
@@ -43,6 +45,22 @@ create_learners <- function(task, vec_learners = NULL){
   }
 
   learners <- lrns(lst)
+
+  if (hpo == TRUE) {
+    for (l in learners) {
+      id <- sub(paste0(cr, "."), "",l$id)
+
+      if (id == "kknn") {
+        l$param_set$values$k <- to_tune(1, task$nrow / 10)
+      } else if (id == "svm") {
+        l$param_set$values$cost <- to_tune(1e-10, 10, logscale = TRUE)
+      } else if (id == "ranger") {
+        l$param_set$values$min.node.size <- to_tune(1, 5)
+      } else if (id == "xgboost") {
+        l$param_set$values$eta <- to_tune(0,1)
+      }
+    }
+  }
 
   return(learners)
 }
