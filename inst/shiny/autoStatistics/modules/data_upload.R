342962ff6d34eca52d0fae9b225261ff87cda6bd
elMrt <Martin.Gruber.1112@gmail.com>
2021-11-18 23:50:03 +0100

    catch error

--- inst/shiny/autoStatistics/modules/data_upload.R
+++ inst/shiny/autoStatistics/modules/data_upload.R
@@ -69,19 +69,6 @@ data_upload_server <- function(id){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     # upload data
     observeEvent(input$file, {
       target_column(NULL)
@@ -113,11 +100,14 @@ data_upload_server <- function(id){
         },
         error = function(cond){
           output$error_message_upload <- renderUI({autoStatistics::render_error("FILETOTABLE", cond)})
+          user_data(NULL)
         },
         warn = function(cond){
           output$error_message_upload <- renderUI({autoStatistics::render_error("FILETOTABLE", cond)})
+          user_data(NULL)
         }
       )
+      req(user_data())
     # detect factor columns
       tryCatch(
         {
