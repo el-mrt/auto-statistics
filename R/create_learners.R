ee6edf70adc5e258e949cca4d70649c03182af38
Lukas Unterschütz <unterschuetz.lukas@gmail.com>
2021-11-19 11:12:31 +0100

    wrote terminator function outsourced search_space to own function

--- R/create_learners.R
+++ R/create_learners.R
@@ -2,12 +2,11 @@
 #'
 #' @param task mlr3 task
 #' @param vec_learners vector of choosen learners
-#' @param hpo choose if searchspaces for hyperparameter optimization should be initialized
 #'
 #' @examples
 #' create_learners(tsk("iris"))
 #' create_learners(tsk("bike_sharing"))
-#' create_learners(task = tsk("german_credit"), vec_learners = c("rf"), hpo = FALSE)
+#' create_learners(task = tsk("german_credit"), vec_learners = c("rf"))
 #'
 #' @return list of mlr3 learners
 #'
@@ -45,22 +44,6 @@ create_learners <- function(task, vec_learners = NULL, hpo = TRUE){
   }
 
   learners <- lrns(lst)
-
-  if (hpo == TRUE) {
-    for (l in learners) {
-      id <- sub(paste0(cr, "."), "",l$id)
-
-      if (id == "kknn") {
-        l$param_set$values$k <- to_tune(1, round(task$nrow / 10))
-      } else if (id == "svm") {
-        l$param_set$values$cost <- to_tune(1e-10, 10, logscale = TRUE)
-      } else if (id == "ranger") {
-        l$param_set$values$min.node.size <- to_tune(1, 5)
-      } else if (id == "xgboost") {
-        l$param_set$values$eta <- to_tune(0,1)
-      }
-    }
-  }
 
   return(learners)
 }
