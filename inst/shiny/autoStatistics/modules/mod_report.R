de96282365bd9915260c44ce49f427cdd6ac86f5
Martin <Martin.Gruber.1112@gmail.com>
2021-12-22 14:23:18 +0100

    test

--- inst/shiny/autoStatistics/modules/mod_report.R
+++ inst/shiny/autoStatistics/modules/mod_report.R
@@ -65,17 +65,18 @@ report_server <- function(id, user_data){
       cur_report$path <- NULL
       if(input$report_type == "custom"){
 
-        # get template and set up temp file path
-        path_template <- system.file("inst", "shiny", "autoStatistics","www", "rep_templ_custom_html.Rmd", package="autoStatistics")
-        temp_report <- file.path(tempdir(), "temp_report.Rmd")
-        # copy file
-        file.copy(path_template, temp_report, overwrite = TRUE)
-
-        temp_report <- rmarkdown::render(input = path_template, output_dir = file.path(tempdir(), "temp_report_html.Rmd"), envir = new.env(parent = globalenv()), params = list(custom_plot = report_plots$custom_report))
-        cur_report$path <- c(
-          cur_report$path,
-          "html" = temp_report
-        )
+        print(getwd())
+        print(file.path(tempdir(), "temp_report.Rmd"))
+      #   # get template and set up temp file path
+      #   path_template <- system.file("inst", "shiny", "autoStatistics","www", "rep_templ_custom_html.Rmd", package="autoStatistics")
+      #   temp_report <- file.path(tempdir(), "temp_report.Rmd")
+      #   # copy file
+      #   file.copy(path_template, temp_report, overwrite = TRUE)
+      #
+      #   temp_report <- rmarkdown::render(input = path_template, output_dir = file.path(tempdir(), "temp_report_html.Rmd"), envir = new.env(parent = globalenv()), params = list(custom_plot = report_plots$custom_report))
+      #   cur_report$path <- c(
+      #     cur_report$path,
+      #     "html" = temp_report)
       }
 
 
